04ff34d0fcc8f704a73fbc8407c5c7723632ae58
Yuriy Belikov <yuriybelikov1@gmail.com>
2026-09-24 20:39:53 +0300

    Added comments to the target structure

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -9,10 +9,10 @@ constexpr int maxRandom = 100;
 struct S {
   int i; // 4 bytes
   long long l; // 8 bytes
-  short s; // 2 bytes
   double d; // 8 bytes
   bool b; // 1 byte
-
+  short s; // 2 bytes
+  
   bool operator<(const S &s) const { return this->i < s.i; }
 };
 
